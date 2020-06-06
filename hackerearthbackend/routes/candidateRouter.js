089063d3176e52144379133e65503e5d70ed756d
jenny456 <dsouzajenisha82@gmail.com>
2020-06-06 17:59:54 +0530

    changes completed.....

--- hackerearthbackend/routes/candidateRouter.js
+++ hackerearthbackend/routes/candidateRouter.js
@@ -9,7 +9,6 @@ const candidateRouter = express.Router();
 
 candidateRouter.use(bodyParser.json());
 
-
 candidateRouter.route('/')
 .options(cors.corsWithOptions,authenticate.verifyUser, (req, res) => { res.sendStatus(200); })
 .get(cors.cors,(req,res,next) => {
@@ -31,18 +30,5 @@ candidateRouter.route('/')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.put(cors.corsWithOptions,(req, res, next) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /dishes');
-})
-.delete(cors.corsWithOptions,(req, res, next) => {
-    Candidates.remove({})
-    .then((resp) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(resp);
-    }, (err) => next(err))
-    .catch((err) => next(err));    
-});
 
 module.exports=candidateRouter;
